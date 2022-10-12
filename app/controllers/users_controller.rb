08f8b77191311084179d9512c96b1df5428552c7
Chemobo <jkchemobo@gmail.com>
2022-10-13 01:58:19 +0300

    creates routes of index and show for users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+    def index
+        render json: User.all, status: :ok
+    end
+
+    def show
+        user = User.find_by(id:params[:id])
+        if user
+            render json: user, status: :ok
+        else 
+            render json: {error: "user not found"}, status: :not_found
+        end  
+
+    end
 end
