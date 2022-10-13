b52964cecef8b7da4ff7700dadce910ad954c30a
Chemobo <jkchemobo@gmail.com>
2022-10-13 22:47:28 +0300

    sets up react app components

---
+++ client/src/Navbar.js
@@ -0,0 +1,14 @@
+const Navabar = () => {
+    return (
+        <nav className="navbar">
+            <h1>The Dog Shop</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/">New Dog</a>
+            </div>
+            </nav>
+        
+    );
+}
+ 
+export default Navabar;
