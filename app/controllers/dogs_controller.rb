060241dc4a8e7004dc7cc376d329ee8b01ca2b02
Chemobo <jkchemobo@gmail.com>
2022-10-13 03:30:04 +0300

    generated dog serializer, created show and create routes for dogs

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -6,4 +6,24 @@ class DogsController < ApplicationController
     render json: dogs, status: :ok
     end
 
+    def show
+        dog = Dog.find_by(id:params[:id])
+        if dog
+            render json: dog, status: :ok
+        else 
+            render json: {error: "dog not found"}, status: :not_found
+        end 
+    end    
+
+    def create
+        dog = Dog.create(dog_params)
+        render json: dog, status: :created
+    end
+       
+    private
+    
+    def dog_params
+    params.permit(:breed, :image, :likes, :price)
+    end
+        
 end
